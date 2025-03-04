3980bff9b957747ad32ce45ea00ab0d830046b17
Vishal kumar <143444741+kitovish01@users.noreply.github.com>
2025-03-05 01:27:16 +0530

    Update conn.js

--- database/conn.js
+++ database/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-// mongodb+srv://kitovish:728290Vk##@mongo.11gsfk4.mongodb.net/?retryWrites=true&w=majority&appName=mongo
+
 //mongodb+srv://sharma092005:asdf1ghjkl@cluster0.cjshox2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 mongoose
   .connect(
